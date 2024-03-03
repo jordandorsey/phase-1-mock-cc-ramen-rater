fe3c4fbced44b8011dbc7f262cfdf2730c35a942
JordanDorsey <jordandorsey04@gmail.com>
2024-03-03 15:10:35 -0500

    index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-// write your code here 
+function renderOneRamen(ramen){
+    let card = document.createElement('li')
+    card.className = 'card'
+    card.innerHTML = `
+    <img src = "${ramen.imageurl}">
+    <div id = "${ramen.id}
+    `
+}
+
+const { json } = require("stream/consumers");
+
+function getAlltheRamens() {
+    fetch ('http://localhost:3000/characters')
+    .then(res => res.json())
+    .then(Ramensdata => (Ramensdata.forEach(Ramen => renderOneRamen(Ramen))))
+    console.log('fetch before returns')
+}
+
+
+function intiliaze(){
+    getAlltheRamens()
+    console.log('fetch after returns')
+}
+
+intiliaze()
